866db1821a0d0784d4a54aefd2f3dcff89bc1197
maaz54 <mak.creativedeveloper@gmail.com>
2025-04-28 00:44:25 +0500

    feat: Added Summary and comments

--- Assets/Solutions/Task1_ObjectPooling/Scripts/Controller/ObjectPoolController.cs
+++ Assets/Solutions/Task1_ObjectPooling/Scripts/Controller/ObjectPoolController.cs
@@ -7,12 +7,23 @@ using UnityEngine.UI;
 
 namespace ObjectPooling
 {
+    /// <summary>
+    /// responsible for object pooling, handles the spawning and releasing of objects, and interacts with a UI button.
+    /// </summary>
     public class ObjectPoolController : MonoBehaviour
     {
+        // Reference to the UI button to trigger object spawning.
         [SerializeField] Button spawnButton;
+
+        // The object to be pooled.
         [SerializeField] MonoBehaviour poolPrefab;
+
+        // Time in seconds for which the objects
         [SerializeField] float holdTime;
+
+        // Number of objects to spawn at once.
         [SerializeField] int spawnQuantity;
+
         GenericObjectPool<MonoBehaviour> objectPooler;
 
         private void Start()
@@ -22,23 +33,35 @@ namespace ObjectPooling
             AddButtonLisener();
         }
 
+        /// <summary>
+        /// Adds the listener to the spawn button click event.
+        /// </summary>
         private void AddButtonLisener()
         {
             spawnButton.onClick.AddListener(OnSpawnButton);
         }
 
+        /// <summary>
+        /// Removes all listeners from the spawn button, ensuring no duplicate event triggers.
+        /// </summary>
         private void RemoveButtonLisenter()
         {
             spawnButton.onClick.RemoveAllListeners();
         }
 
+        /// <summary>
+        /// Handles the spawn button click event.
+        /// It removes the button listener, starts the spawning and releasing process, and re-adds the listener after completion.
+        /// </summary>
         private void OnSpawnButton()
         {
             RemoveButtonLisenter();
             _ = SpawnAndRelease(holdTime);
         }
 
-
+        /// <summary>
+        /// Spawns a set quantity of objects, positions them randomly within a circle, and releases them after a specified delay.
+        /// </summary>
         private async Task SpawnAndRelease(float holdTime)
         {
 
--- Assets/Solutions/Task1_ObjectPooling/Scripts/Pooling/PoolObject.cs
+++ Assets/Solutions/Task1_ObjectPooling/Scripts/Pooling/PoolObject.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 namespace ObjectPooling
 {
+    /// <summary>
+    /// This is just a demo class which is used to spawn and release objects in the pool.
+    /// It doesn't hold any functionality but serves as a placeholder for pooling.
+    /// </summary>
     public class PoolObject : MonoBehaviour
     {
     }
